7e72b238acdbba5d05ad49b92a0fac7e72b1bb0f
Benjamin Feldman <benjamin.feldman@datadoghq.com>
2024-12-02 18:21:18 +0100

    add part 2

--- aoc2024/day1.clj
+++ aoc2024/day1.clj
@@ -1,28 +1,44 @@
-(ns aoc2024.day1
-  (:require [clojure.string :as str]))
+(ns day1
+  (:require [clojure.string]))
 
-(defn calculate-difference []
-  (let [
-        lines (str/split-lines (slurp "resources/day1/input.txt"))
-        
-        [left-nums right-nums] 
-        (reduce 
-          (fn [[lefts rights] line]
-            (let [[left right] (str/split line #"\s{3,}")]
-              [(conj lefts left) 
-               (conj rights right)]))
-          [[] []]
-          lines)
-        
+(defn get-split-lines [filepath]
+  (let [lines (clojure.string/split-lines (slurp filepath))]
+    (reduce
+     (fn [[lefts rights] line]
+       (let [[left right] (clojure.string/split line #"\s{3,}")]
+         [(conj lefts left)
+          (conj rights right)]))
+     [[] []]
+     lines)))
+
+;; Part 1
+
+(defn first-part [filepath]
+  (let [[left-nums right-nums] (get-split-lines filepath)
         sorted-left (sort left-nums)
         sorted-right (sort right-nums)
-        
-        differences (map #(Math/abs (- (parse-long %1) 
-                                     (parse-long %2))) 
-                        sorted-left 
-                        sorted-right)]
-    
+        differences (map #(Math/abs (- (parse-long %1)
+                                       (parse-long %2)))
+                         sorted-left
+                         sorted-right)]
     (reduce + differences)))
 
-(defn -main [& args]
-  (println (calculate-difference)))
+;; Part 2
+
+(defn counter-hashmap [coll]
+  (reduce (fn [counts item]
+            (update counts item (fnil inc 0)))
+          {}
+          coll))
+
+(defn second-part [filepath]
+(  let [
+        [left-nums right-nums] (get-split-lines filepath) 
+        right-counter (counter-hashmap right-nums)
+
+        similarities (map #(* (parse-long %) (right-counter % 0)) left-nums)
+        ]
+ (reduce + similarities)
+ ))
+
+
